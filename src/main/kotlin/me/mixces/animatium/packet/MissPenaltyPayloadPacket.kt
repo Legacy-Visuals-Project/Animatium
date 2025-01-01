a62c50978b0670962d0e0d16b79a9cfaf7728219
lowercasebtw <jejojora2@gmail.com>
2025-01-01 02:03:57 -0500

    Simplify MissPenaltyPayloadPacket

--- src/main/kotlin/me/mixces/animatium/packet/MissPenaltyPayloadPacket.kt
+++ src/main/kotlin/me/mixces/animatium/packet/MissPenaltyPayloadPacket.kt
@@ -6,20 +6,16 @@ import net.minecraft.util.Identifier
 
 class MissPenaltyPayloadPacket(val value: Boolean) : CustomPayload {
     companion object {
-        val CODEC = CustomPayload.codecOf<PacketByteBuf, MissPenaltyPayloadPacket>(
-            MissPenaltyPayloadPacket::write
-        ) { buf ->
-            MissPenaltyPayloadPacket(buf.readBoolean())
-        }
-
+        val CODEC = CustomPayload.codecOf(null, MissPenaltyPayloadPacket::read)
         val PAYLOAD_ID =
             CustomPayload.Id<MissPenaltyPayloadPacket>(Identifier.of("animatium:disable_swing_miss_penalty"))
+
+        private fun read(buffer: PacketByteBuf): MissPenaltyPayloadPacket {
+            return MissPenaltyPayloadPacket(buffer.readBoolean())
+        }
     }
 
-    private fun write(buffer: PacketByteBuf) {
-    }
-
-    override fun getId(): CustomPayload.Id<out CustomPayload?>? {
+    override fun getId(): CustomPayload.Id<out CustomPayload> {
         return PAYLOAD_ID
     }
 }
