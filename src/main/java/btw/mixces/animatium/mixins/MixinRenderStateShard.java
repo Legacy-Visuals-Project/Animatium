fb60dc353ec4fb1a8232888e8bb49fdcfe2d134b
lowercasebtw <jejojora2@gmail.com>
2025-01-09 01:41:40 -0500

    Small syntax cleanup

--- src/main/java/btw/mixces/animatium/mixins/MixinRenderStateShard.java
+++ src/main/java/btw/mixces/animatium/mixins/MixinRenderStateShard.java
@@ -9,17 +9,17 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(RenderStateShard.class)
 public class MixinRenderStateShard {
     @ModifyExpressionValue(method = "setupGlintTexturing", at = @At(value = "CONSTANT", args = "doubleValue=8.0"))
-    private static double animatium$disableSpeedMultiplier(double original) {
+    private static double animatium$oldGlintSpeed(double original) {
         return AnimatiumConfig.instance().getOldGlintSpeed() ? 1.0D : original;
     }
 
     @ModifyExpressionValue(method = "setupGlintTexturing", at = @At(value = "CONSTANT", args = "floatValue=110000.0"))
-    private static float animatium$oldSpeedValue$4873(float original) {
+    private static float animatium$oldGlintSpeed$4873(float original) {
         return AnimatiumConfig.instance().getOldGlintSpeed() ? 4873.0F : original;
     }
 
     @ModifyExpressionValue(method = "setupGlintTexturing", at = @At(value = "CONSTANT", args = "floatValue=30000.0"))
-    private static float animatium$oldSpeedValue$3000(float original) {
+    private static float animatium$oldGlintSpeed$3000(float original) {
         return AnimatiumConfig.instance().getOldGlintSpeed() ? 3000.0F : original;
     }
 }
