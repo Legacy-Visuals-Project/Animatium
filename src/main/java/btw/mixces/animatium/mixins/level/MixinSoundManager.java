8cc1e38e827dc8411865f40e2707d9b9a9148f92
lowercasebtw <jejojora2@gmail.com>
2025-01-30 00:30:25 -0500

    Fix setting "Disable modern combat sounds"

--- src/main/java/btw/mixces/animatium/mixins/level/MixinSoundManager.java
+++ src/main/java/btw/mixces/animatium/mixins/level/MixinSoundManager.java
@@ -26,36 +26,33 @@ package btw.mixces.animatium.mixins.level;
 import btw.mixces.animatium.AnimatiumClient;
 import btw.mixces.animatium.config.AnimatiumConfig;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
-import net.minecraft.core.Holder;
-import net.minecraft.sounds.SoundEvent;
+import net.minecraft.client.resources.sounds.SoundInstance;
+import net.minecraft.client.sounds.SoundEngine;
+import net.minecraft.client.sounds.SoundManager;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.List;
 
-@Mixin(Level.class)
-public abstract class MixinLevel {
-    // TODO/NOTE: Find a better way
+@Mixin(SoundManager.class)
+public abstract class MixinSoundManager {
     @Unique
-    private static final List<SoundEvent> animatium$ignoreSounds = List.of(
-            SoundEvents.PLAYER_ATTACK_KNOCKBACK,
-            SoundEvents.PLAYER_ATTACK_SWEEP,
-            SoundEvents.PLAYER_ATTACK_CRIT,
-            SoundEvents.PLAYER_ATTACK_STRONG,
-            SoundEvents.PLAYER_ATTACK_WEAK,
-            SoundEvents.PLAYER_ATTACK_NODAMAGE
+    private static final List<ResourceLocation> animatium$ignoreSounds = List.of(
+            SoundEvents.PLAYER_ATTACK_KNOCKBACK.location(),
+            SoundEvents.PLAYER_ATTACK_SWEEP.location(),
+            SoundEvents.PLAYER_ATTACK_CRIT.location(),
+            SoundEvents.PLAYER_ATTACK_STRONG.location(),
+            SoundEvents.PLAYER_ATTACK_WEAK.location(),
+            SoundEvents.PLAYER_ATTACK_NODAMAGE.location()
     );
 
-    @WrapWithCondition(method = "playSeededSound(Lnet/minecraft/world/entity/player/Player;DDDLnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FFJ)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;playSeededSound(Lnet/minecraft/world/entity/player/Player;DDDLnet/minecraft/core/Holder;Lnet/minecraft/sounds/SoundSource;FFJ)V"))
-    private boolean animatium$disableModernCombatSounds(Level instance, @Nullable Player player, double x, double y, double z, Holder<SoundEvent> soundEventHolder, SoundSource soundSource, float volume, float pitch, long unknown) {
+    @WrapWithCondition(method = "play", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sounds/SoundEngine;play(Lnet/minecraft/client/resources/sounds/SoundInstance;)V"))
+    private boolean animatium$disableModernCombatSounds(SoundEngine instance, SoundInstance soundInstance) {
         if (AnimatiumClient.getEnabled() && AnimatiumConfig.instance().getDisableModernCombatSounds()) {
-            return !animatium$ignoreSounds.contains(soundEventHolder.value());
+            return !animatium$ignoreSounds.contains(soundInstance.getLocation());
         } else {
             return true;
         }
