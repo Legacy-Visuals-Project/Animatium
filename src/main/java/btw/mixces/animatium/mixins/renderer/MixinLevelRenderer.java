77484b8533e472c1a36ebb6670a090b7b0d3475d
Tellinq <59412384+Tellinq@users.noreply.github.com>
2025-03-26 23:08:16 -0400

    Undo the recent comment out - this branch is intended to fix

--- src/main/java/btw/mixces/animatium/mixins/renderer/MixinLevelRenderer.java
+++ src/main/java/btw/mixces/animatium/mixins/renderer/MixinLevelRenderer.java
@@ -122,18 +122,18 @@ public abstract class MixinLevelRenderer {
         }
     }
 
-//    @WrapOperation(method = "method_62214", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;endBatch(Lnet/minecraft/client/renderer/RenderType;)V", ordinal = 16))
-//    private void animatium$legacyGlintRendering$endBatch(MultiBufferSource.BufferSource instance, RenderType renderType, Operation<Void> original) {
-//        original.call(instance, renderType);
-//        if (AnimatiumClient.isEnabled() && AnimatiumConfig.instance().glintRendering) {
-//            instance.endBatch(LegacyGlintType.ITEM_GLINT_LAYER);
-//            instance.endBatch(LegacyGlintType.ITEM_GLINT_2ND_LAYER);
-//            instance.endBatch(LegacyGlintType.ITEM_GLINT_TRANSLUCENT_LAYER);
-//            instance.endBatch(LegacyGlintType.ITEM_GLINT_TRANSLUCENT_2ND_LAYER);
-//            instance.endBatch(LegacyGlintType.ENTITY_GLINT_LAYER);
-//            instance.endBatch(LegacyGlintType.ENTITY_ARMOR_GLINT_LAYER);
-//        }
-//    }
+    @WrapOperation(method = "method_62214", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;endBatch(Lnet/minecraft/client/renderer/RenderType;)V", ordinal = 16))
+    private void animatium$legacyGlintRendering$endBatch(MultiBufferSource.BufferSource instance, RenderType renderType, Operation<Void> original) {
+        original.call(instance, renderType);
+        if (AnimatiumClient.isEnabled() && AnimatiumConfig.instance().glintRendering) {
+            instance.endBatch(LegacyGlintType.ITEM_GLINT_LAYER);
+            instance.endBatch(LegacyGlintType.ITEM_GLINT_2ND_LAYER);
+            instance.endBatch(LegacyGlintType.ITEM_GLINT_TRANSLUCENT_LAYER);
+            instance.endBatch(LegacyGlintType.ITEM_GLINT_TRANSLUCENT_2ND_LAYER);
+            instance.endBatch(LegacyGlintType.ENTITY_GLINT_LAYER);
+            instance.endBatch(LegacyGlintType.ENTITY_ARMOR_GLINT_LAYER);
+        }
+    }
 
     @Unique
     private GpuTexture animatium$blankTexture = null;
