0e969a7917305bdba322bfff023e367485bc9c87
lowercasebtw <ignzoudy@gmail.com>
2026-01-03 05:39:35 -0500

    Fix widget hover text color

--- src/main/java/org/visuals/legacy/animatium/mixins/v1/gui/hover_text_color/MixinAbstractButton_LegacyTextHoverColor.java
+++ src/main/java/org/visuals/legacy/animatium/mixins/v1/gui/hover_text_color/MixinAbstractButton_LegacyTextHoverColor.java
@@ -25,10 +25,15 @@
 
 package org.visuals.legacy.animatium.mixins.v1.gui.hover_text_color;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.gui.components.AbstractButton;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.visuals.legacy.animatium.Animatium;
+import org.visuals.legacy.animatium.config.AnimatiumConfig;
 
 @Mixin(AbstractButton.class)
 public abstract class MixinAbstractButton_LegacyTextHoverColor extends AbstractWidget {
@@ -36,13 +41,14 @@ public abstract class MixinAbstractButton_LegacyTextHoverColor extends AbstractW
 		super(x, y, width, height, message);
 	}
 
-	// TODO: 1.21.11
-    /*@ModifyConstant(method = "renderWidget", constant = @Constant(intValue = 0xFFFFFFFF))
-    private int animatium$renderWidget$old$textColor(int constant) {
-        if (Animatium.isEnabled() && AnimatiumConfig.instance().screen.legacyWidgetHoverTextColor) {
-            return !active ? 0xFFE0E0E0 : (isHoveredOrFocused() ? 0xFFFFFFA0 : 0xFFE0E0E0);
-        } else {
-            return constant;
-        }
-    }*/
+	// TODO: Maybe find the core place to do this better in 1.21.11+?
+	@WrapOperation(method = "renderDefaultLabel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/AbstractButton;getMessage()Lnet/minecraft/network/chat/Component;"))
+	private Component animatium$renderWidget$old$textColor(final AbstractButton instance, final Operation<Component> original) {
+		final Component component = original.call(instance);
+		if (Animatium.isEnabled() && AnimatiumConfig.instance().screen.legacyWidgetHoverTextColor) {
+			return component.copy().withColor(!active ? 0xFFE0E0E0 : (isHoveredOrFocused() ? 0xFFFFFFA0 : 0xFFE0E0E0));
+		} else {
+			return component;
+		}
+	}
 }
